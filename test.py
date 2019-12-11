96891a0f4c5a0a004572912a2e0ac2670928a2de
Manish Vishnoi (Student) <mvishnoi@en4110007l.fulton.ad.asu.edu>
2019-12-11 16:44:17 -0700

    Kmeans done

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[59]:
+# In[1]:
 
 
 import pandas as pd
+import numpy as np
 from joblib import load
 import warnings
 import logging
@@ -15,58 +16,53 @@ import argparse
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
-# In[27]:
+# In[2]:
 
 
 parser = argparse.ArgumentParser(description='argument parsing')
 parser.add_argument('--file', type=str, default='data/test.csv')
 args = parser.parse_args()
 file = args.file
-print("Test file successfully : {}".format(str(file)))
-df = pd.read_csv(file)
+print("Test file successfully uploaded: {}".format(str(file)))
+test_file = pd.read_csv(file)
 
 
-# In[114]:
+# In[ ]:
 
 
 def test():
     print("=======================")
     print("Uploading Feature file")
     print("=======================")
-    x = pd.read_csv('data/features_file.csv')
-    test_file = pd.read_csv('data/test.csv')
     print("=======================")
     print("Extracting Features from test file")
     print("=======================")
     featured = feature_collection(test_file)
-    X_train = pd.read_csv('train_data.csv')
+    print(featured.shape)
     scaler = load("scaler.save")
     featured = scaler.transform(featured)
     print("=======================")
     print("Loading models")
     print("=======================")
-    SVM = load('models/SVM.joblib')
-    LR = load('models/Logistic.joblib')
-    RFC = load('models/random_forest.joblib')
-    ADA = load('models/ADA.joblib')
+    kmeans_pro = load("kmeans.bin")
     print("=======================")
     print("Predicting based on four models")
     print("=======================")
-    cols=['SVM','LR','RFC','ADA']
-    output = pd.DataFrame(columns=cols)
-    output['SVM'] = SVM.predict(featured)
-    output['LR'] = LR.predict(featured)
-    output['RFC'] = RFC.predict(featured)
-    output['ADA'] = ADA.predict(featured)
+    y_test_df = pd.DataFrame(featured)
+    y_test_df['Predicted'] = kmeans_pro.predict(featured)
+    carb_matrix = ["10-20","20-30","30-40","40-50","60-70","50-60","70-80","80-90","90-100","0-10"]
+    final = []
+    for x in y_test_df.iterrows():
+        final.append(carb_matrix[np.int64(x[1]['Predicted'])])
+    y_test_df['carb_values'] = final
     print("=======================")
     print("Output saved in output.csv file in current dir")
     print("=======================")
-    output.to_csv('output.csv')
-    print(output)
-    return output
+    y_test_df.to_csv('output.csv')
+    return y_test_df
 
 
-# In[115]:
+# In[ ]:
 
 
 def feature_collection(test_file):
@@ -79,7 +75,6 @@ def feature_collection(test_file):
     impute(f)
     assert f.isnull().sum().sum() == 0
     f=f[['0__spkt_welch_density__coeff_2', '0__fft_coefficient__coeff_1__attr_"abs"','0__partial_autocorrelation__lag_1','0__autocorrelation__lag_1','0__autocorrelation__lag_2']]
-    f.to_csv('test_fe.csv')
     return f
 
 
@@ -87,3 +82,4 @@ def feature_collection(test_file):
 
 if __name__=="__main__":
     test()
+
--- train.py
+++ train.py
@@ -1,80 +1,70 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[21]:
+# In[1]:
 
 
+import pandas as pd
+import numpy as np
+from pprint import pprint
+from sklearn.impute import SimpleImputer
+from tsfresh import extract_features, select_features,feature_selection
+from tsfresh.utilities.dataframe_functions import impute
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.pipeline import make_pipeline
+from joblib import load, dump
 import warnings
 warnings.filterwarnings('ignore')
 import logging
 logging.getLogger('tsfresh').setLevel(logging.ERROR)
 logging.getLogger('sklearn').setLevel(logging.ERROR)
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore",category=DeprecationWarning)
-
-    import pandas as pd
-    import numpy as np
-    from pprint import pprint
-    from sklearn.impute import SimpleImputer
-    from tsfresh import extract_features, select_features,feature_selection
-    from tsfresh.utilities.dataframe_functions import impute
-    from sklearn.model_selection import train_test_split, RandomizedSearchCV
-    from sklearn.preprocessing import StandardScaler, MinMaxScaler
-    from sklearn.pipeline import make_pipeline
-    from sklearn.linear_model import LogisticRegression as LR
-    from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-    from joblib import load, dump
-    from sklearn.metrics import roc_auc_score as AUC, accuracy_score as accuracy, precision_score, recall_score, f1_score
-    from sklearn.svm import SVC as SVM
-    from sklearn.model_selection import cross_val_score, cross_validate
-    from sklearn.ensemble import AdaBoostClassifier as ABC
-    from sklearn.gaussian_process.kernels import RBF
-    from sklearn.tree import DecisionTreeClassifier as DTC
-    from sklearn.model_selection import GridSearchCV
-    from sklearn import svm
-    from sklearn import preprocessing
-    from sklearn.linear_model import LogisticRegressionCV
-    from scipy.stats import randint as sp_randint
-    from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-    from sklearn.tree import DecisionTreeClassifier
+from sklearn.cluster import KMeans
 
 
-# In[22]:
-
+# In[2]:
 
 
 def train():
     result = pd.DataFrame()
-#     Extracting Data for meal class
-
     for x in range(5):
-        d = pd.read_csv('data/mealData'+str(x+1)+'.csv', header = None,error_bad_lines=False)
-        # d['y']= 1
-        result = pd.concat([result,d])
-    
-#     Extracting data for no meal class
-    # for x in range(5):
-    #     d = pd.read_csv('data/Nomeal'+str(x+1)+'.csv', header = None,error_bad_lines=False)
-    #     d['y']= 0
-    #     result = pd.concat([result,d])
-        
-#         Imputing for NaN value removal
+            d = pd.read_csv('data/mealData'+str(x+1)+'.csv', header = None,error_bad_lines=False)
+            f = pd.read_csv('data/mealAmountData'+str(x+1)+'.csv', header = None,error_bad_lines=False)
+            f.values.tolist()
+            final_val =[]
+            for x in range(d.shape[0]):
+                final_val.append(f[0][x])
+            d['carbo'] = final_val
+            result = pd.concat([result,d])
     result = impute_data(result)
-    
-#     Renaming Target column to dataframe
     result = pd.DataFrame(result)
     columns = list(result.columns)
     columns.pop()
     columns.append('target')
     result.columns = columns
-    
-#     Extracting features and writing into files
     features = feature_extract(result,'data/features_file.csv')
-#     Cross fold validation and Training models
-    # classifier()
+    data = pd.read_csv("features_file.csv")
+    data = data.iloc[:, :-1]
+    data = data.drop(['id'],axis=1)
+    print(data.head())
+    scaler = MinMaxScaler().fit(data)
+    data = scaler.transform(data)
+    print(data.shape)
+    dump(scaler,"scaler.save")
+    kmeans = KMeans(n_clusters=10, random_state=0).fit(data)
+    dump(kmeans,"kmeans.bin")
 
 
-# In[23]:
+# In[3]:
+
+
+def impute_data(result):
+    imp_mean = SimpleImputer(missing_values=np.nan,strategy='mean')
+    imp_mean.fit(result)
+    return(imp_mean.transform(result))
+
+
+# In[4]:
 
 
 def feature_extract(result, filename):
@@ -92,112 +82,10 @@ def feature_extract(result, filename):
     f['y'] = y  
     f.to_csv( filename, index = None )
     return f
-    
 
 
-# In[24]:
+# In[5]:
 
 
-def impute_data(result):
-    imp_mean = SimpleImputer(missing_values=np.nan,strategy='mean')
-    imp_mean.fit(result)
-    return(imp_mean.transform(result))
-
-
-# In[25]:
-
-
-def classifier():
-    data = pd.read_csv("data/features_file.csv")
-    X_train, X_test, y_train, y_test = train_test_split(data.iloc[:,:-1], data['y'], test_size=0.33, random_state=42)
-    X_train.to_csv('train_data.csv')
-    scaler = MinMaxScaler().fit(X_train)
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-    dump(scaler,"scaler.save")
-    SVM(X_train,X_test,y_train,y_test)
-    LR(X_train,X_test,y_train,y_test)
-    RFC(X_train,X_test,y_train,y_test)
-    ADA(X_train,X_test,y_train,y_test)
-
-
-# In[26]:
-
-
-def SVM(X_train,X_test,y_train,y_test):
-    parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
-    svc = svm.SVC(gamma="scale")
-    clf = GridSearchCV(svc, parameters, cv=10)
-    clf.fit(X_train, y_train)
-    best = clf.best_estimator_
-    best = load('models/SVM.joblib')
-    y_pred = best.predict(X_test)
-    # test_file = pd.read_csv('test_fe.csv')
-#     print(best.predict(test_file))
-    print("AUC of SVM {:.4f}, Accuracy of SVM is {:.4f}, Precision is {:.4f}, Recall is {:.4f}, F1 score is {:.4f}".format(AUC(y_test,y_pred), accuracy(y_test,y_pred),precision_score(y_test, y_pred), recall_score(y_test, y_pred),f1_score(y_test,y_pred) ))
-    return best
-
-
-# In[27]:
-
-
-def LR(X_train,X_test,y_train,y_test):
-    lr = LogisticRegressionCV(cv=5, random_state=0, multi_class='auto').fit(X_train, y_train)
-    lr=load('models/Logistic.joblib')
-    y_pred = lr.predict(X_test)
-    print("AUC LR {:.4f}, Accuracy of LR is {:.4f}, Precision is {:.4f}, Recall is {:.4f}, F1 score is {:.4f}".format(AUC(y_test,y_pred), accuracy(y_test,y_pred),precision_score(y_test, y_pred), recall_score(y_test, y_pred),f1_score(y_test,y_pred) ))
-    
-
-
-# In[28]:
-
-
-def RFC(X_train,X_test,y_train,y_test):
-    clf = RandomForestClassifier(n_estimators=20)
-    param_grid = {"max_depth": [3, None],
-                  "max_features": [1, 2, 4],
-                  "min_samples_split": [2, 3, 10],
-                  "bootstrap": [True, False],
-                  "criterion": ["gini", "entropy"]}
-    grid_search = GridSearchCV(clf, param_grid=param_grid, cv=10)
-    grid_search.fit(X_train, y_train)
-    grid_search=load('models/random_forest.joblib')
-    y_pred = grid_search.predict(X_test)
-    print("AUC of RFC {:.4f}, Accuracy of RFC is {:.4f}, Precision is {:.4f}, Recall is {:.4f}, F1 score is {:.4f}".format(AUC(y_test,y_pred), accuracy(y_test,y_pred),precision_score(y_test, y_pred), recall_score(y_test, y_pred),f1_score(y_test,y_pred) ))
-
-
-# In[29]:
-
-
-def ADA(X_train,X_test,y_train,y_test):
-    param_grid = {"base_estimator__criterion" : ["gini", "entropy"],
-                  "base_estimator__splitter" :   ["best", "random"],
-                  "n_estimators": [1, 2]
-                 }
-
-    # DTC = DecisionTreeClassifier(random_state = 11, max_features = "auto", class_weight = "balanced",max_depth = None)
-    dtc = DecisionTreeClassifier(max_depth = 12)
-    ABC = AdaBoostClassifier(base_estimator = dtc)
-
-    # run grid search
-    grid_search_ABC = GridSearchCV(ABC, param_grid=param_grid, cv=5)
-    grid_search_ABC.fit(X_train, y_train)
-    grid_search_ABC = load('models/ADA.joblib')
-    y_pred = grid_search_ABC.predict(X_test)
-    print("AUC of ADA {:.4f}, Accuracy of ADA is {:.4f}, Precision is {:.4f}, Recall is {:.4f}, F1 score is {:.4f}".format(AUC(y_test,y_pred), accuracy(y_test,y_pred),precision_score(y_test, y_pred), recall_score(y_test, y_pred),f1_score(y_test,y_pred) ))
-    return grid_search_ABC
-
-
-# In[30]:
-def main():
-  train()  
-
-# train()
 if __name__=="__main__":
-    main()
-
-# In[ ]:
-
-
-
-
+    train()
